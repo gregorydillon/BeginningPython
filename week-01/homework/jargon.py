f269fedcde2b593a52789be2cdea1cf3dffa0012
Daniel Nowak <dnowak@technoratimedia.com>
2014-01-22 18:23:17 -0800

    Week one Slides update

---
+++ week-01/homework/jargon.py
@@ -0,0 +1,12 @@
+"""
+In this class we are going to write a game called jargon. For this week we
+want to create a simple start screen for our game.
+
+Write a program that clears the terminal screen and prints out the following.
+
+*************************************************
+**                                             **
+**              Welcome to Jargon              **
+**                                             **
+*************************************************
+"""
---
+++ week-01/labs/lab0102_answer.py
@@ -0,0 +1,20 @@
+steak_price = 18.95
+manhattan_price = 9.95
+salad_price = 12.95
+print "Steak"
+print steak_price
+print "Manhattan"
+print manhattan_price
+print "Salad"
+print salad_price
+
+subtotal = steak_price + manhattan_price + salad_price
+tax = subtotal * 0.0825
+grand_total = subtotal + tax
+
+print "Subtotal"
+print subtotal
+print "Tax"
+print tax
+print "Grand Total"
+print grand_total
